1e7a42ee04406102d7df370248c5bdfbc709ace3
mrk <papalagui010@gmail.com>
2015-10-26 11:53:21 +0100

    added OSM map to main page

---
+++ resources/assets/js/mapHelper.js
@@ -0,0 +1,18 @@
+
+/* init map */
+function initMap(){
+    map = new ol.Map({
+        target:'map',
+        renderer:'canvas',
+    	view: new ol.View({    		
+    		center: ol.proj.transform([-0.1973237, 51.5412081], 'EPSG:4326', 'EPSG:3857'),
+    		zoom:14
+    	})
+    });
+    
+    var newLayer = new ol.layer.Tile({
+    source: new ol.source.OSM()});
+
+	map.addLayer(newLayer);	
+		
+}
