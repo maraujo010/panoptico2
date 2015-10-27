d301f977606ab458458d226b2eea7fa3957c2349
mrk <papalagui010@gmail.com>
2015-10-27 19:38:21 +0100

    add OSM map to main page with angular

---
+++ resources/assets/js/mapApp/Services/dataService.js
@@ -0,0 +1,19 @@
+angular
+    .module('mapApp')
+    .service('dataService', dataService);
+
+function dataService() {
+
+    var data = {
+        Map: ''
+    };
+
+    return {
+        getMap: function () {
+            return data.Map;
+        },
+        setMap: function (map) {
+            data.Map = map;
+        }
+    };
+}
