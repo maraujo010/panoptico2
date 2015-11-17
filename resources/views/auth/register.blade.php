a02bf5473c8f19f92c85b8f212582194724cf5bc
mrk <papalagui010@gmail.com>
2015-11-17 02:10:13 +0100

    modified the registation and login views

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,42 @@
+@extends('layout.pages')
+@section('content')
+
+<div class="row register-content">
+	<div class="col-md-3 col-md-offset-2 register-form">		
+		<form method="POST" action="/register">
+			{!! csrf_field() !!}
+			<fieldset>		 	
+				<div class="form-group">
+			 		<label for="nickname" class="control-label">Nickname</label>				 				 				       
+					<input type="text" name="nickname" class="form-control" value="{{ old('name') }}">						
+				</div>		
+				<div class="form-group">
+					<label for="email" class="control-label" placeholder="Email">Email</label>						
+					<input type="email" name="email" class="form-control" value="{{ old('email') }}">						
+				</div>
+				<div class="form-group">
+					<label for="email_confirmation" class="control-label" placeholder="Email">Email Confirmation</label>						
+					<input type="email" name="email_confirmation" class="form-control" value="{{ old('email') }}">						
+				</div>
+				<div class="form-group">
+					<label for="password" class="control-label">Password</label>						
+					<input type="password" name="password" class="form-control">						
+				</div>
+				<div class="form-group">
+					<label for="password_confirmation" class="control-label">Password Confirmation</label>						
+					<input type="password" name="password_confirmation" class="form-control">
+				</div>
+				<div class="form-group">						
+					<button type="submit" class="btn btn-primary">Register</button>						
+				</div>	
+			</fieldset>					
+		</form>				
+	</div>
+	<div class="col-md-4 col-md-offset-1 register-text">
+		<p>
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+		</p>
+	</div>
+</div>
+
+@stop
